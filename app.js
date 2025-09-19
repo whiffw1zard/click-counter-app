e0abdf0df6e02647a3a28b05d68f69e3f26a85fb
whiffw1zard <bro3125@hotmail.com>
2025-09-19 08:56:41 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// Select elements from the DOM
+
+// Initialize scores
+
+// Add event listeners to buttons
+
+// Function to get computer's choice
+
+// Function to determine the winner of a round
+
+// Function to play a round
